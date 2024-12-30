4aed77fb2ad98becd7e3aef2d80f1a437c867cd3
marcofelipi <marco.felipi@hotmail.com>
2024-12-30 11:52:29 -0300

    botões e função on pressed

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,44 @@ main() {
 }
 
 class PerguntaApp extends StatelessWidget {
+  void responder() {
+    print('Pergunta respondida.');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
               title: Text('Perguntas'),
               backgroundColor: Color.fromARGB(255, 21, 242, 21)),
-          body: Text('Olá flutter.')),
+          body: Column(
+            children: [
+              Text(perguntas[0]),
+              ElevatedButton(
+                child: Text('Resposta 1'),
+                onPressed: responder,
+              ),
+              ElevatedButton(
+                child: Text('Resposta 2'),
+                onPressed: responder,
+              ),
+              ElevatedButton(
+                  child: Text('Resposta 3'),
+                  onPressed: () {
+                    print('Pergunta respondida');
+                  }),
+              ElevatedButton(
+                child: Text('Resposta 4'),
+                onPressed: () => print('Pergunta respondida'),
+              ),
+            ],
+          )),
     );
   }
 }
